01882f4f6131df72b904acccf78e7c5091bb64c9
kimng1003 <kimnagyeong50@gmail.com>
2023-06-26 11:14:46 +0900

    파일 합치기

---
+++ welpair/src/main/java/com/hielectro/welpair/inventory/model/dto/CategoryDTO.java
@@ -0,0 +1,18 @@
+package com.hielectro.welpair.inventory.model.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+//@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CategoryDTO {
+
+    private String categoryCode;
+    private String categoryName;
+    private String refCategoryCode;
+
+
+}
