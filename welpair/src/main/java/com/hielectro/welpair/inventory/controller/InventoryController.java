cc1d974c4528dfcaa83b576080a79e612b54a6aa
kimng1003 <kimnagyeong50@gmail.com>
2023-06-26 12:54:14 +0900

    파일 합치기중

--- welpair/src/main/java/com/hielectro/welpair/inventory/controller/InventoryController.java
+++ welpair/src/main/java/com/hielectro/welpair/inventory/controller/InventoryController.java
@@ -6,14 +6,12 @@ import com.hielectro.welpair.inventory.model.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/HTML")
+@RequestMapping("/inventory")
 public class InventoryController {
     private final InventoryService inventoryService;
 
@@ -22,6 +20,11 @@ public class InventoryController {
         this.inventoryService = inventoryService;
     }
 
+
+    @GetMapping("{url}")
+    public String adminInventory(@PathVariable String url) {
+        return "admin/inventory/" + url;
+    }
 
 
     /**
@@ -42,7 +45,7 @@ public class InventoryController {
         System.out.println("totalInvenAmount = " + totalInvenAmount);
         System.out.println("alertStock = " + alertStock);
 
-        return "HTML/admin/admin_inventory";
+        return "admin/inventory/admin_inventory";
     }
 
 
@@ -63,16 +66,15 @@ public class InventoryController {
      * 1. 재고현황 페이지
      * 1-2. 하단부 상품 코드 검색 시 해당 상품의 간단한 정보 출력
      */
-    @PostMapping("admin/admin_inventory")
+    @PostMapping("inventory/admin_inventory")
     public String searchProductByCode(Model model, @RequestParam("productCode") String searchCode) {
         System.out.println("-------------컨트롤러 1-2 -------------");
         System.out.println("searchCode = " + searchCode);
         List<ProductDTO> productList = inventoryService.searchProductByCode(searchCode);
         System.out.println("==================== 1-2 =============");
         model.addAttribute("productList", productList);
-        return "HTML/admin/admin_inventory";
+        return "inventory/admin_inventory";
     }
-
 
     /**
      * 재고관리 메뉴 (ng)
@@ -80,10 +82,10 @@ public class InventoryController {
      * 2-1. 등록할 상품 검색
      *      상품코드, 상품명, 카테고리 선택 후 검색 시 등록 대상 리스트 출력
      */
-    @PostMapping("admin/admin_inventory_regist")
-    public String stockRegistSerch(Model model, @RequestParam("productCode") String productCode,
-                                               @RequestParam("productName") String productName,
-                                               @RequestParam("category") String categoryName){
+    @PostMapping("inventory/admin_inventory_regist")
+    public String stockRegistSerch(Model model, @ModelAttribute("productCode") String productCode,
+                                   @ModelAttribute("productName") String productName,
+                                   @ModelAttribute("category") String categoryName){
         System.out.println("-------------컨트롤러 2-1 -------------");
         System.out.println("productCode = " + productCode);
         System.out.println("productName = " + productName);
@@ -93,6 +95,6 @@ public class InventoryController {
         model.addAttribute("stockList", stockList);
 
         System.out.println("stockList = " + stockList);
-        return "HTML/admin/admin_inventory_regist";
+        return "admin/inventory/admin_inventory_register";
     }
 }
