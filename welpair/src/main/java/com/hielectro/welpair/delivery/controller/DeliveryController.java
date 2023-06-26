b455d34ad3910a079bd1eb356865e39175755cea
minjihi <alswl9614@gmail.com>
2023-06-26 16:00:35 +0900

    commit

---
+++ welpair/src/main/java/com/hielectro/welpair/delivery/controller/DeliveryController.java
@@ -0,0 +1,16 @@
+package com.hielectro.welpair.delivery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/delivery")
+public class DeliveryController {
+
+    @GetMapping("deliverymain")
+    public String deliveryMain(){
+
+        return "admin/delivery/delivery_main";
+    }
+}
