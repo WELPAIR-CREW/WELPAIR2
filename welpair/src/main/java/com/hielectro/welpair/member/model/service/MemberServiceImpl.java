1d36c2facfd54224bade646b7de21b197c15b9b3
hayeong-yu <seongtan2020@gmail.com>
2023-06-27 13:19:20 +0900

    실행오류수정

--- welpair/src/main/java/com/hielectro/welpair/member/model/service/MemberServiceImpl.java
+++ welpair/src/main/java/com/hielectro/welpair/member/model/service/MemberServiceImpl.java
@@ -4,9 +4,11 @@ package com.hielectro.welpair.member.model.service;
 import com.hielectro.welpair.member.model.dao.MemberMapper;
 import com.hielectro.welpair.member.model.dto.MemberDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+    @Service
 public class MemberServiceImpl implements MemberService {
 
     private final MemberMapper memberMapper;
