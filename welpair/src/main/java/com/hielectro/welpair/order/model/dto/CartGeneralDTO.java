288c00a60c5b1fe4a2375193a56fe58d39273eeb
HDhye <hdhye89@gmail.com>
2023-06-28 10:02:04 +0900

    main

--- welpair/src/main/java/com/hielectro/welpair/order/model/dto/CartGeneralDTO.java
+++ welpair/src/main/java/com/hielectro/welpair/order/model/dto/CartGeneralDTO.java
@@ -2,7 +2,6 @@ package com.hielectro.welpair.order.model.dto;
 
 
 import com.hielectro.welpair.inventory.model.dto.ProductDTO;
-import com.hielectro.welpair.sellproduct.model.dto.SellImageDTO;
 import com.hielectro.welpair.sellproduct.model.dto.SellItemPageDTO;
 import com.hielectro.welpair.sellproduct.model.dto.SellPageDTO;
 import com.hielectro.welpair.sellproduct.model.dto.SellProductDTO;
