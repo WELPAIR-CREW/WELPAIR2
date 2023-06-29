45b148246c5832f65361be16f38b3cf274093654
김희승 <heski0709@gmail.com>
2023-06-29 19:39:47 +0900

    FEAT: 리뷰 목록 출력 #21

--- welpair/src/main/java/com/hielectro/welpair/sellproduct/model/service/SellProductService.java
+++ welpair/src/main/java/com/hielectro/welpair/sellproduct/model/service/SellProductService.java
@@ -3,12 +3,15 @@ package com.hielectro.welpair.sellproduct.model.service;
 import java.util.List;
 import java.util.Map;
 
+import com.hielectro.welpair.board.model.dto.ReviewManagerDTO;
 import com.hielectro.welpair.sellproduct.model.dto.SellProductDTO;
+import com.hielectro.welpair.sellproduct.model.dto.SellProductDetailDTO;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface SellProductService {
     public int sellProductSearchCount(Map<String, String> search);
-    public List<SellProductDTO> selectProductList(Map<String, String> productId);
+    public List<SellProductDetailDTO> selectProductList(Map<String, String> productId);
+    List<ReviewManagerDTO> selectReviewList();
 
     @Transactional(rollbackFor = {Exception.class})
     int delete(List<String> request) throws Exception;
