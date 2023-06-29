65b157bb42851b41c588588fc528d502e6f87ef5
hayeong-yu <seongtan2020@gmail.com>
2023-06-29 15:41:49 +0900

    페이징 수정...페이지번호 세트 바꿔주기 전

--- welpair/src/main/java/com/hielectro/welpair/member/controller/MemberController.java
+++ welpair/src/main/java/com/hielectro/welpair/member/controller/MemberController.java
@@ -9,9 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/member")
@@ -26,37 +30,36 @@ public class MemberController {
 
 
     //1. 회원조회 - 회원목록
-    @GetMapping("member-view")
-    public String getMemberList(@RequestParam(name="currentPage", defaultValue="1") int currentPage, Model model) {
+    @GetMapping("/member-view")
+    public ModelAndView getMemberList(HttpServletRequest request, @RequestParam(required = false) String searchCondition, @RequestParam(required = false) String searchValue
+            , @RequestParam(value="currentPage", defaultValue="1") int currentPage, ModelAndView model) {
                                 //int currentPage: URL로 전달되는 현재 페이지 번호로 URL에 제공되지 않으면 1로 설정됨)
 
-        //1-4.
+        System.out.println("test----------------- ");
+        Map<String, String> searchMap = new HashMap<>();
+        searchMap.put("searchCondition", searchCondition);
+        searchMap.put("searchValue", searchValue);
+
+        int totalMemberCount = memberService.totalMemberCount(searchMap); //총 항목 수(검색 조건 적용)
         int itemsPerPage = 10; //페이지당 항목 수
-        List<MemberDTO> memberList = memberService.getMemberList(currentPage, itemsPerPage);
-
-        //1-1. 쿼리결과를 받아 DTO리스트에 담고 모델에 추가
-//        List<MemberDTO> memberList = memberService.getMemberList();
-        model.addAttribute("memberList", memberList);
-
-        //1-2. 페이징처리를 위해 현재 페이지 번호를 모델에 추가
-        int totalPages = getTotalPages(memberList);
+        int displayPageCount = 5; //표시할 페이지 번호 수
 
 
-        //1-3. 페이지번호들을 동적으로 보여주기위해 추가(5개씩)------------------------
-        int displayPageCount = 5; //원하는 표시 페이지 수
-        int startPage = Math.max(1, currentPage - displayPageCount/2);
-        int endPage = Math.min(startPage + displayPageCount - 1, totalPages);
-        List<Integer> pages = new ArrayList<>(); //페이지번호들의 리스트
-        for(int page = startPage; page <= endPage; page++) {
-            pages.add(page);
+        SelectCriteria selectCriteria = null;
+
+        if(searchCondition != null && !"".equals(searchCondition)) {
+            selectCriteria = Pagenation.getSelectCriteria(currentPage, totalMemberCount, itemsPerPage, displayPageCount, searchCondition, searchValue);
+        } else {
+            selectCriteria = Pagenation.getSelectCriteria(currentPage, totalMemberCount, itemsPerPage, displayPageCount);
         }
-        model.addAttribute("pages", pages);
-        //----------------------------------------------------------------
 
-        model.addAttribute("currentPage", currentPage);
-        model.addAttribute("totalPages", totalPages);
+        List<MemberDTO> memberList = memberService.getMemberList(selectCriteria);
 
-        return "admin/member/member-view";
+        //쿼리결과를 받아 DTO리스트에 담고 모델에 추가
+        model.addObject("memberList", memberList);
+        model.addObject("selectCriteria", selectCriteria);
+        model.setViewName("admin/member/member-view");
+        return model;
     }
 
 
@@ -82,20 +85,5 @@ public class MemberController {
     public String getPointHistoryList() {
         return "admin/member/member-givePointHistory1";
     }
-
-
-
-    //1-2. 페이징처리를 위해 전체페이지 수를 계산하는 메서드
-    private int getTotalPages(List<MemberDTO> memberList) {
-        int totalItems = memberList.size(); //총 항목 수
-        int itemsPerPage = 10; //페이지당 항목 수
-        int totalPages = (int) Math.ceil((double) totalItems / itemsPerPage); //총 페이지 수
-        System.out.println("총 페이지 수 : " + totalPages);
-        return totalPages;
-    }
-
-
-
-
 
 }
--- welpair/src/main/java/com/hielectro/welpair/member/model/dao/MemberMapper.java
+++ welpair/src/main/java/com/hielectro/welpair/member/model/dao/MemberMapper.java
@@ -1,4 +1,5 @@
 package com.hielectro.welpair.member.model.dao;
+import com.hielectro.welpair.member.controller.SelectCriteria;
 import com.hielectro.welpair.member.model.dto.MemberDTO;
 import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
@@ -6,12 +7,12 @@ import java.util.Map;
 
 @Mapper
 public interface MemberMapper {
-//    List<MemberDTO> getMemberList();
-//    List<MemberDTO> getMemberList(int startRow, int endRow);
-    List<MemberDTO> getMemberList(Map<String, Integer> startAndEndRow);
+
+    List<MemberDTO> getMemberList(SelectCriteria selectCriteria);
 
 
-    int totalMemberCount();
+    int totalMemberCount(Map<String, String> searchMap);
 
     int expiredMemberCount();
+
 }
