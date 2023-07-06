295099b2f042124dd916637caf981a6bc0f3f95b
ssay <designjay31@gmail.com>
2023-07-06 12:28:07 +0900

    regist

---
+++ welpair/src/main/java/com/hielectro/welpair/regist/controller/MemberRegistException.java
@@ -0,0 +1,10 @@
+package com.hielectro.welpair.common;
+
+public class MemberRegistException extends Throwable {
+
+    public MemberRegistException() {}
+
+    public MemberRegistException(String msg) {
+        super(msg);
+    }
+}
---
+++ welpair/src/main/java/com/hielectro/welpair/regist/model/dto/MemberRoleDTO.java
@@ -0,0 +1,18 @@
+package com.hielectro.welpair.regist.model.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AuthorityDTO {
+
+
+    private String memNo;
+    private String authCode;
+
+
+
+}
