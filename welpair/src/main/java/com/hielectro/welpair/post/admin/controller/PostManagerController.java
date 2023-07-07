bbc6db024f48ef36561958b00ff5133781b0c5e8
ssay <designjay31@gmail.com>
2023-07-07 17:15:13 +0900

    login mapper teble delet update

--- welpair/src/main/java/com/hielectro/welpair/post/admin/controller/PostManagerController.java
+++ welpair/src/main/java/com/hielectro/welpair/post/admin/controller/PostManagerController.java
@@ -1,9 +1,13 @@
 package com.hielectro.welpair.post.admin.controller;
 
 
+import com.hielectro.welpair.post.admin.model.dao.AdminBoardDAO;
+import com.hielectro.welpair.post.admin.model.dto.AdminBoardDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,4 +18,22 @@ public class PostManagerController {
     public String PostWrite(Model model){
         return "/post/admin/board_write";
     }
+
+    @PostMapping("/admin/board-write")
+    public String BoardSave(@ModelAttribute AdminBoardDTO adminBoardDTO){
+
+        System.out.println(adminBoardDTO);
+
+        return null;
+
+    }
+
+
+
+
+
+
+
 }
+
+
