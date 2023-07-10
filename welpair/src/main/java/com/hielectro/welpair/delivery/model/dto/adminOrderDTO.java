77c76911cb8cf34e29416b09bcb390518094fe1c
minjihi <alswl9614@gmail.com>
2023-07-10 18:02:49 +0900

    배송테이블 수정,  DTO 추가

--- welpair/src/main/java/com/hielectro/welpair/delivery/model/dto/adminOrderDTO.java
+++ welpair/src/main/java/com/hielectro/welpair/delivery/model/dto/adminOrderDTO.java
@@ -1,6 +1,9 @@
 package com.hielectro.welpair.delivery.model.dto;
 
+import com.hielectro.welpair.order.model.dto.OrderDTO;
 import lombok.*;
+
+import java.sql.Timestamp;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -8,7 +11,7 @@ import lombok.*;
 @Setter
 @ToString
 public class adminOrderDTO {
-
+//T_ORDER
     private String orderNo;
     private String orderDate;
     private int totalPrice;
--- welpair/src/main/java/com/hielectro/welpair/delivery/model/service/deliveryService.java
+++ welpair/src/main/java/com/hielectro/welpair/delivery/model/service/deliveryService.java
@@ -3,6 +3,7 @@ package com.hielectro.welpair.delivery.model.service;
 import com.hielectro.welpair.delivery.model.dao.deliveryMapper;
 import com.hielectro.welpair.delivery.model.dto.DeliveryDTO;
 import com.hielectro.welpair.delivery.model.dto.DriverDTO;
+import com.hielectro.welpair.delivery.model.dto.OrderProductDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,9 +18,9 @@ public class deliveryService {
     }
 
 //    DAO에 만들어진 메소드 호출, 비지니스 로직이 필요하다면 작성
-    public List<DeliveryDTO> deliveryDelivery() {
+    public List<OrderProductDTO> deliveryDelivery() {
         return delivery.deliveryDelivery();
-
-
     }
+
+
 }
