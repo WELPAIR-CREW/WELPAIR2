cff6326538841fdb9ea5ddd81840b2f078080cd2
ssay <designjay31@gmail.com>
2023-07-13 12:00:17 +0900

    post commit

--- welpair/src/main/java/com/hielectro/welpair/post/model/service/AdminBoardService.java
+++ welpair/src/main/java/com/hielectro/welpair/post/model/service/AdminBoardService.java
@@ -2,6 +2,7 @@ package com.hielectro.welpair.post.model.service;
 
 import com.hielectro.welpair.post.controller.BoardException;
 import com.hielectro.welpair.post.model.dto.AdminBoardDTO;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface AdminBoardService {
@@ -13,5 +14,8 @@ public interface AdminBoardService {
 
     public void MemberWriteSave(AdminBoardDTO adminBoardDTO) throws  BoardException;
 
+
+    @Transactional
+    AdminBoardDTO selectBoardDetail(String boardNo);
 
 }
